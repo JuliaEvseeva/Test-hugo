68504bf2af07bcd67efbb07caef15cbbd385723b
Julia Yevseieva <julia.evseeva@teamdev.com>
2022-06-14 08:45:13 +0000

    Updated 'cloudcannon.config.js' via CloudCannon

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -6,7 +6,23 @@ module.exports = {
         const slug = filters.slugify(parsed.title || '');
         return `/careers/${year}/${slug}/`;
       },
-      output: true
+      output: true,
+      add_options: [
+        {
+          name: 'Add Post',
+          editor: 'content'
+        },
+        {
+          name: 'Add Draft',
+          editor: 'content',
+          schema: draft
+        },
+      ],
+      schemas: {
+        draft: {
+          path: 'schemas/test.md',
+        }
+      }
     }
   }
 }
